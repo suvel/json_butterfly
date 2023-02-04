2a0de02712425007abaeadef1beadaff7db79833
suvel <find.suvel@gmail.com>
2023-02-04 12:41:04 +0530

    Added Formatter component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import './App.css';
+import ReactJson from 'react-json-view';
 
 function App() {
-  return <div className="App">Hello World!</div>;
+  return (
+    <div className="App">
+      <ReactJson
+        src={{
+          message: {
+            name: 'suvel',
+            age: '56',
+          },
+        }}
+      />
+    </div>
+  );
 }
 
 export default App;
