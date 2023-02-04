ff3e537cac5bffe52b86c95e80523b846379d71e
suvel <find.suvel@gmail.com>
2023-02-04 15:22:26 +0530

    Added Error boundry

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import { useState } from 'react';
+import ErrorBoundary from './ErrorBoundary';
 
 function App() {
   const [json, setJson] = useState('{}');
@@ -17,27 +18,29 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <Box>
-        <Grid container spacing={2}>
-          <Grid item xs={6} maxHeight={'100vh'} overflow={'auto'}>
-            <TextField
-              id="outlined-textarea"
-              label="Paste your json here to visualize"
-              placeholder={`{"name":"ramesh"}`}
-              multiline
-              value={json}
-              onChange={(e) => setJson(e.target.value)}
-              fullWidth
-            />
-            <Button onClick={handelFormate}>Formate</Button>
+    <ErrorBoundary>
+      <div className="App">
+        <Box>
+          <Grid container spacing={2}>
+            <Grid item xs={6} maxHeight={'100vh'} overflow={'auto'}>
+              <TextField
+                id="outlined-textarea"
+                label="Paste your json here to visualize"
+                placeholder={`{"name":"ramesh"}`}
+                multiline
+                value={json}
+                onChange={(e) => setJson(e.target.value)}
+                fullWidth
+              />
+              <Button onClick={handelFormate}>Formate</Button>
+            </Grid>
+            <Grid item xs={6} maxHeight={'100vh'} overflow={'auto'}>
+              <ReactJson src={submittedJson} />
+            </Grid>
           </Grid>
-          <Grid item xs={6} maxHeight={'100vh'} overflow={'auto'}>
-            <ReactJson src={submittedJson} />
-          </Grid>
-        </Grid>
-      </Box>
-    </div>
+        </Box>
+      </div>
+    </ErrorBoundary>
   );
 }
 
